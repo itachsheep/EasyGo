97c9c38fe24f153f36a934971c1cbb23648a8374
taowei <taowei@skyworth.com>
2017-12-06 19:46:05 +0800

    学习attr自定义属性

---
+++ app/src/main/java/com/java/easygo/view/MainActivity.java
@@ -0,0 +1,19 @@
+package com.java.easygo.view;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by SDT14324 on 2017/12/5.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+}
